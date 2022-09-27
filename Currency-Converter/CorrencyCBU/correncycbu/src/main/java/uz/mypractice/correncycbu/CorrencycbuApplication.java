ec5c6eb3fba11ba9e70f456714e334fa710d6651
Khoja <figetti@mail.ru>
2022-09-27 15:24:50 +0500

    123

---
+++ Currency-Converter/CorrencyCBU/correncycbu/src/main/java/uz/mypractice/correncycbu/CorrencycbuApplication.java
@@ -0,0 +1,13 @@
+package uz.mypractice.correncycbu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrencycbuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorrencycbuApplication.class, args);
+    }
+
+}
