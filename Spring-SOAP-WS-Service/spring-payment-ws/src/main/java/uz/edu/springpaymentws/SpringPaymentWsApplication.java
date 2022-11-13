c70bd51c7b564a3d4afd2bf871c4ec9d8323634f
Hamdamboy <hamdamboy.urunov@gmail.com>
2022-11-13 13:47:03 +0500

    SOAP Project

---
+++ Spring-SOAP-WS-Service/spring-payment-ws/src/main/java/uz/edu/springpaymentws/SpringPaymentWsApplication.java
@@ -0,0 +1,13 @@
+package uz.edu.springpaymentws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaymentWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPaymentWsApplication.class, args);
+    }
+
+}
---
+++ Spring-SOAP-WS-Service/spring-payment-ws/src/test/java/uz/edu/springpaymentws/SpringPaymentWsApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.edu.springpaymentws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPaymentWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
